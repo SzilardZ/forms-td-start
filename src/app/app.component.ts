af32dca879d5b07f0a5f3b2d7227bee68a5ab935
SzilardZ <szilard.zr@gmail.com>
2019-05-03 17:47:08 +0200

    adding validation the check user input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -7,6 +7,8 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') variableName: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
