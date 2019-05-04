12df9174cb42cb219d29aba7d768bf7f421441b7
SzilardZ <szilard.zr@gmail.com>
2019-05-04 14:36:05 +0200

    using form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,43 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') variableName: NgForm;
+  @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'teacher';
-  answer: '';
+  answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     userName: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: '',
+    // })
+    this.signupForm.form.patchValue({
+      userData: {
+        userName: suggestedName,
+      }})
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
